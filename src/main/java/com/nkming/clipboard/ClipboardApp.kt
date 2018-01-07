2c0214a5089fb2e87ac829c87c72c28a0653c89b
Ming Ming <mint2648@gmail.com>
2018-01-07 23:15:56 +0800

    Fix default pref values not populated

--- src/main/java/com/nkming/clipboard/ClipboardApp.kt
+++ src/main/java/com/nkming/clipboard/ClipboardApp.kt
@@ -2,6 +2,7 @@ package com.nkming.clipboard
 
 import android.app.Application
 import android.content.Context
+import android.preference.PreferenceManager
 import android.support.v4.content.ContextCompat
 import com.nkming.utils.Log
 
@@ -17,6 +18,7 @@ class ClipboardApp : Application()
 		super.onCreate()
 		context = applicationContext
 		initLog()
+		initDefaultPref()
 
 		ContextCompat.startForegroundService(context,
 				ClipboardService.buildStartIntent(context))
@@ -26,5 +28,11 @@ class ClipboardApp : Application()
 	{
 		Log.isShowDebug = BuildConfig.DEBUG
 		Log.isShowVerbose = BuildConfig.DEBUG
+	}
+
+	private fun initDefaultPref()
+	{
+		PreferenceManager.setDefaultValues(this, getString(R.string.pref_file),
+				Context.MODE_PRIVATE, R.xml.preference, false)
 	}
 }
